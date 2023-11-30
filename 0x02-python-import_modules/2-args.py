8d2649ef7f57bad0af47cc9310afc32360fecc2a
Innocent Anozie <iygeal@gmail.com>
2023-11-30 22:03:32 +0100

    Fixed pycodestyle issues

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     argc = len(sys.argv)
     if argc == 1:
-        print("{} arguments.".format(argc -1))
+        print("{} arguments.".format(argc - 1))
     elif argc == 2:
-        print("{} argument:".format(argc- 1))
+        print("{} argument:".format(argc - 1))
     else:
         print("{} arguments:".format(argc - 1))
 
