d7b74df8fd10eff0605f6bf518b76006f3ed0e15
Innocent Anozie <iygeal@gmail.com>
2023-12-01 09:40:32 +0100

    Typo correction on print line

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
+
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
@@ -11,4 +12,4 @@ if __name__ == "__main__":
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2](a, b)]))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
