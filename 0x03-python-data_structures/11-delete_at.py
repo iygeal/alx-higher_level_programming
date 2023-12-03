18b118b6ece306e762c57d52db3f3a8e42e852a0
Innocent Anozie <iygeal@gmail.com>
2023-12-03 23:10:00 +0100

    Modified code to use del() function

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,5 +2,5 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):  # Check given task conditions
         return my_list
-    new_list = my_list[:idx] + my_list[idx+1:]  # Make new_list without idx
-    return new_list  # Return new_list without idx
+    del my_list[idx]  # Make new_list without idx
+    return my_list  # Return new_list without idx
