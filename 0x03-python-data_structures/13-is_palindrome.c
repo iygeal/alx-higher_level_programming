28ebb40cce71ce9c9dfdd447b97d37cd10579490
Innocent Anozie <iygeal@gmail.com>
2023-12-04 11:52:58 +0100

    Rewrote code for task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,25 +1,15 @@
 #include "lists.h"
 #include <stddef.h>
-
+#include <stdbool.h>
 /**
- * is_palindrome - Function that checks if a singly linked list is a palindrome
- * @head: Pointer to a pointer to first element in the list
- * Return: 0 if it is not a palindrome, 1 if it is
+ * reverse_list - Function that reverses a singly linked list
+ * @head: Pointer to a pointer to the first node in the list
+ * Return: Returns pointer to the first node of the reversed list
  */
-int is_palindrome(listint_t **head)
+void reverse_list(listint_t **head)
 {
-	listint_t *slow = *head;
-	listint_t *fast = *head;
-
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
-	while (fast->next != NULL && fast->next->next != NULL)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-	}
 	listint_t *prev = NULL;
-	listint_t *current = slow->next;
+	listint_t *current = *head;
 	listint_t *next = NULL;
 
 	while (current != NULL)
@@ -29,15 +19,50 @@ int is_palindrome(listint_t **head)
 		prev = current;
 		current = next;
 	}
-	listint_t *left = *head;
-	listint_t *right = prev;
+	*head = prev;
+}
 
-	while (right != NULL)
+/**
+ * is_palindrome - Function that checks if a singly linked list is a palindrome
+ * @head: Pointer to a pointer to the first node in the list
+ * Return: 1 if it is a palindrome, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *aux = *head;
+	listint_t *dup = NULL;
+
+	while (true)
 	{
-		if (left->n != right->n)
-			return (0);
-		left = left->next;
-		right = right->next;
+		fast = fast->next->next;
+		if (fast == NULL)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (fast->next == NULL)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
 	}
-	return (1);
+	reverse_list(&dup);
+
+	while (dup != NULL && aux != NULL)
+	{
+		if (aux->n == dup->n)
+		{
+			dup = dup->next;
+			aux = aux->next;
+		}
+		else
+			return (0);
+	}
+	if (dup == NULL)
+		return (1);
+	else
+		return (0);
 }
