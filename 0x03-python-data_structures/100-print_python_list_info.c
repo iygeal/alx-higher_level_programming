902f6d39b578eb1c3da571fa9a9f4c881fe1c1de
Innocent Anozie <iygeal@gmail.com>
2023-12-05 01:09:05 +0100

    Betty fix

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,35 +8,35 @@
  **/
 void print_python_list_info(PyObject *p)
 {
-    long int size;
-    int i;
-    PyListObject *obj;
+	long int size;
+	int i;
+	PyListObject *obj;
 
-    if (!PyList_Check(p) || !p)
-    {
-        fprintf(stderr, "Invalid Python List\n");
-        return;
-    }
+	if (!PyList_Check(p) || !p)
+	{
+		fprintf(stderr, "Invalid Python List\n");
+		return;
+	}
 
-    size = PyList_Size(p);
-    obj = (PyListObject *)p;
+	size = PyList_Size(p);
+	obj = (PyListObject *)p;
 
-    if (size < 0)
-    {
-        fprintf(stderr, "Failed to get size of Python List\n");
-        return;
-    }
+	if (size < 0)
+	{
+		fprintf(stderr, "Failed to get size of Python List\n");
+		return;
+	}
 
-    printf("[*] Size of the Python List = %li\n", size);
-    printf("[*] Allocated = %li\n", obj->allocated);
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
 
-    for (i = 0; i < size; i++)
-    {
-        if (!obj->ob_item[i])
-        {
-            fprintf(stderr, "Failed to get Python List item\n");
-            return;
-        }
-        printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
-    }
+	for (i = 0; i < size; i++)
+	{
+		if (!obj->ob_item[i])
+		{
+			fprintf(stderr, "Failed to get Python List item\n");
+			return;
+		}
+	printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+	}
 }
