8e8349f50cd67161d05c73f07f3fc3a798cbe7df
Innocent Anoziee <iygeal@gmail.com>
2023-12-06 17:48:51 +0100

    Pycodestyle fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     square_matrix = []
     for i in matrix:
-      square_matrix.append(list(map(lambda x: x * x, i)))
+        square_matrix.append(list(map(lambda x: x * x, i)))
     return square_matrix
