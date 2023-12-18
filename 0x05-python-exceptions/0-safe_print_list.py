2778af4a236bf6c8b082616d4e1a007764cc5755
Innocent Anozie <iygeal@gmail.com>
2023-12-18 15:35:03 +0100

    Function that prints given index of a list using try and except

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pythin3
+def safe_print_list(my_list=[], x=0):
+    print_x = 0
+
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            print_x = print_x + 1
+    except IndexError:
+        pass
+    finally:
+        print()
+    return (print_x)
