0c46e5dcc4676b031d089572025902052a8b428a
Innocent Anozie <iygeal@gmail.com>
2023-12-19 20:04:53 +0100

    Fixed pycodestyle errors

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ Module providing an empty definition of
-	class named 'Square'
+    class named 'Square'
 """
+
+
 class Square:
-	""" Actual empty definition of the class 'Square'
-	"""
+    """ Actual empty definition of the class 'Square'
+    """
