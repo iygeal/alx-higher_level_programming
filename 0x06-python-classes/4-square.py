35109a83d8980fd831a11817e403621b2e49efb7
Innocent Anozie <iygeal@gmail.com>
2023-12-20 02:16:04 +0100

    Created a class named  Square and reassigns value to private size

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+""" Module providing a definition of
+    class named 'Square'
+"""
+
+
+class Square:
+    """
+    Square class definition
+
+    Attributes:
+    - size (int): The size of the square. Defaults to 0.
+
+    Raises:
+    - TypeError: If size is not an integer.
+    - ValueError: If size is less than 0.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance.
+
+        Parameters:
+        - size (int): The size of the square. Defaults to 0.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Getter method to retrieve the size of the square
+        Returns:
+        - int: Returns the size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size of the square
+        Parameters:
+        - Value (int): The new size value
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Computes the area of the square
+        Returns:
+        - int: The area of current square
+        """
+        return self.__size ** 2
