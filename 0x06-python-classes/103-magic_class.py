f50031c200c825f40be4363a7a900b52cb162e53
Innocent Anozie <iygeal@gmail.com>
2023-12-31 12:07:49 +0100

    Magic circle replicated from bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import math
+"""
+Module of class named "MagicClass" which replicates a bytecode
+with circle methods
+"""
+
+
+class MagicClass:
+    """
+    Definition of the class 'MagicClass'
+    """
+
+    def __inti__(self, radius=0):
+        """
+        Initialization of a MagicClass instance
+
+        Args:
+            radius (float): The radius of a circle. Defaults to 0.
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """
+        Calculates and returns the area of the circle
+        """
+        return self.radius ** 2 * math.pi
+
+    def circumference(self):
+        """
+        Calculates and returns the circumfereence of the cicle
+        """
+        return 2 * math.pi * self.__radius
