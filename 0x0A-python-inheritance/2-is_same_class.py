8b927591a192a1808a175fd4aaa98ac50b92ce3f
Innocent Anozie <iygeal@gmail.com>
2024-01-08 19:52:16 +0100

    Modified task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -17,4 +17,6 @@ def is_same_class(obj, a_class):
               otherwise, False.
 
     """
-    return isinstance(obj, a_class)
+    if isinstance(obj, a_class):
+        return True
+    return False
