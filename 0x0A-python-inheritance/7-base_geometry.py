2fe52fbc7881f56bd2d1c20095f2681a14bb19d2
Innocent Anozie <iygeal@gmail.com>
2024-01-08 21:36:44 +0100

    Class that validates integer

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Module providing a class BaseGeometry with area and integer_validator methods."""
+
+class BaseGeometry:
+    """A class with area and integer_validator methods."""
+
+    def area(self):
+        """Raise an Exception with the message 'area() is not implemented'."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate the value.
+
+        Args:
+            name (str): The name of the value.
+            value: The value to validate.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
--- 0x0A-python-inheritance/tests/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module providing a class MyList that inherits from list."""
-
-class MyList(list):
-    """A class that inherits from list."""
-
-    def print_sorted(self):
-        """Print the list in sorted (ascending) order."""
-        print(sorted(self))
