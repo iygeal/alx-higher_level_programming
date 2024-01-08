d80f4b7a1d11f6cf359fd02655ffcd31ba169391
Innocent Anozie <iygeal@gmail.com>
2024-01-08 20:24:25 +0100

    Function that checks if object is an instance of a sub class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Module providing a function to check if an object is an instance of a class
+that inherited (directly or indirectly) from the specified class.
+"""
+
+
+def inherits_from(obj, a_class):
+    """Check if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        bool: True if the object is an instance of a class that inherited
+              (directly or indirectly) from the specified class;
+              otherwise, False.
+
+    """
+    return issubclass(type(obj), a_class)
