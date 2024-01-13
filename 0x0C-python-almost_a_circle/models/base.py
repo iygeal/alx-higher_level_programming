77301ea8e86d24341019a105163d2220d53360b2
Innocent Anozie <iygeal@gmail.com>
2024-01-13 21:18:25 +0100

    Added a staticmethod to_json_dictionaries to Base class which returns json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Module that represents the Base class.
 """
+import json
 
 
 class Base:
@@ -25,3 +26,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
