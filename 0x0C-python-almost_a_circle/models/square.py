abfdb18bd5e62d4d7bcef0eb727c6d717e3ae66a
Innocent Anozie <iygeal@gmail.com>
2024-01-13 15:20:13 +0100

    Modified Rectangle and Square classes to return dictionary representation of the classes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,6 +19,41 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter method for the size attribute"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter method for the size attribute"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update the Square attributes based on *args and **kwargs"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return the dictionary representation of the Square"""
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
+
     def __str__(self):
         """String representation of the square"""
         return "[Square] ({}) {}/{} - {}".format(
