052f96c6403b6d583db9a1140ac3c9bfed29d62e
Innocent Anozie <iygeal@gmail.com>
2024-03-27 00:21:21 +0100

    Select conditionally and sort

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- This script lists names and scores from second_table where score >= 10
+-- It also sorts by score in descending order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
