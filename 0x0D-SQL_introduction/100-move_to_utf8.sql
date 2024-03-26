0c3f70a6ee51b9e95b68dbca536db05c3bd2f7e0
Innocent Anozie <iygeal@gmail.com>
2024-03-27 00:58:35 +0100

    Updated task 100 for correct behaviour/output

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,6 @@
--- Convert database character set and collation
+-- This script converts the hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+-- It converts the entire database, including the first_table table and the name field in first_table.
+
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert table character set and collation
+USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert specific field character set and collation
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
