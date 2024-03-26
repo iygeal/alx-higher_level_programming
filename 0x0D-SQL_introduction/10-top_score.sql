baa0585e733b69e233b9c270a5a44c696d7d3d81
Innocent Anozie <iygeal@gmail.com>
2024-03-27 00:25:05 +0100

    Update score by referencing name

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- This script lists all records of second_table of a known database
 -- It displays reults sorted by the scores
-SELECT score, name FROM second_table ORDER BY score DESC
+SELECT score, name FROM second_table ORDER BY score DESC;
