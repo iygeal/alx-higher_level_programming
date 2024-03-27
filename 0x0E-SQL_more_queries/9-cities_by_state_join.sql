0bf5ba788e3af535e834e8671a77eb10c3ccc245
Innocent Anozie <iygeal@gmail.com>
2024-03-27 22:45:45 +0100

    Refactored

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -6,7 +6,7 @@
 
 USE hbtn_0d_usa;
 
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
