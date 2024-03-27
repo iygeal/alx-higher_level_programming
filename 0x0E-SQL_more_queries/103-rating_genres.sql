dd3eaac87844d1cdf8581fce8dc91634b04f6fe0
Innocent Anozie <iygeal@gmail.com>
2024-03-27 23:55:07 +0100

    Refactor

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -5,10 +5,11 @@
 -- for shows belonging to each genre
 -- Results are sorted in descending order by the rating sum
 
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating_sum
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = shows.id
-JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_genres.name
-ORDER BY rating_sum DESC;
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings
+ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.id
+ORDER BY rating DESC;
