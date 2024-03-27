782c0a287018619e432260e8e858c7552b671771
Innocent Anozie <iygeal@gmail.com>
2024-03-27 20:21:11 +0100

    Create a server user, give it all server privileges and set the password

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- This scripts creates a server user named user_0d_1
+-- It gives the user all privileges on the server
+-- It then sets its password to user_0d_1_pwd
+-- The script covers the edge case of whether the username already
+-- exists, in which case the script still runs with no error
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
