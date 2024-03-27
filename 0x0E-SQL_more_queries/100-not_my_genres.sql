d09f92a6c7692ee00d0a4d915e9292126a3f5b7e
Innocent Anozie <iygeal@gmail.com>
2024-03-27 23:38:51 +0100

    Major code refactor

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,17 +6,12 @@
 
 SELECT tv_genres.name
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
-GROUP BY tv_genres.name;
-
-SELECT name
+WHERE tv_genres.id NOT IN
+(SELECT tv_genres.id
 FROM tv_genres
-WHERE name NOT IN (
-    SELECT tv_genres.name
-    FROM tv_genres
-    LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-    LEFT JOIN tv_shows ON tv_show_genres.tv_show_id = shows.id
-    WHERE tv_shows.title = 'Dexter'
-);
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter")
+ORDER BY tv_genres.name;
