ec89c63c71e4cfb80a42d2ecc3f844b360fb7932
Innocent Anozie <iygeal@gmail.com>
2024-03-27 23:22:23 +0100

    Typo fix

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,7 +3,7 @@
 -- Shows without linked genres are displayed as NULL in the genre column
 -- Results are sorted in ascending order by the show title and genre name
 
-SELECT tv_shows.title, tv_genres.name AS genre
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
