abfc12b7ea6b81977183e2345f1cbe5b96bd122e
Innocent Anozie <iygeal@gmail.com>
2024-03-27 23:21:09 +0100

    a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- This script lists all shows and their linked genres from the hbtn_0d_tvshows database
+-- It retrieves the title of each show from the tv_shows table and the genre name from the tv_genres table
+-- Shows without linked genres are displayed as NULL in the genre column
+-- Results are sorted in ascending order by the show title and genre name
+
+SELECT tv_shows.title, tv_genres.name AS genre
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
