4b2b9efcb0f93be5163d8fb47944774cd849ad39
Innocent Anozie <iygeal@gmail.com>
2024-03-27 23:49:25 +0100

    TV shows rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -5,6 +5,13 @@
 
 SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating_sum
 FROM tv_shows
-JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
 GROUP BY tv_shows.title
 ORDER BY rating_sum DESC;
+
+-- SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating_sum
+-- FROM tv_shows
+-- INNER JOIN tv_show_ratings
+-- ON tv_shows.id = tv_show_ratings.show_id
+-- GROUP BY tv_shows.id
+-- ORDER BY rating DESC;
