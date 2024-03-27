d7139db0e722773c2de80366e6e718d60cea3967
Innocent Anozie <iygeal@gmail.com>
2024-03-27 01:23:49 +0100

    Code refactored

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,17 +1,13 @@
--- This script retrieves and analyzes temperature data from the 'temperatures' table.
--- It focuses on data from the months of July (7) and August (8), using the 'MONTH' function
--- to extract the month number from the 'date' column for filtering.
--- It then calculates the average temperature for each city during those months and displays
--- the top 3 cities with the highest average temperatures in descending order.
-
 -- Filtering for July and August
 -- Grouping data by city to calculate average temperature
 -- Ordering by average temperature in descending order
 -- Limiting displayed results to the top 3 cities
+-- This script retrieves and analyzes temperature data from the 'temperatures' table.
+-- It focuses on data from the months of July (7) and August (8).
 
-SELECT city, AVG(value) AS average_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-WHERE MONTH(date) IN (7, 8)
+WHERE month=7 OR month=8  -- Filter for July and August
 GROUP BY city
-ORDER BY average_temp DESC
+ORDER BY avg_temp DESC
 LIMIT 3;
