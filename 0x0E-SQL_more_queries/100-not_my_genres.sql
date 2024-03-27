70dfe68b4ff41b0c5e50e9eb2055ed79f7d77f7b
Innocent Anozie <iygeal@gmail.com>
2024-03-27 23:37:58 +0100

    Typo

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,11 +1,13 @@
--- This script lists all genres not linked to the show "Dexter" from the hbtn_0d_tvshows database
--- It retrieves the genres linked to the show "Dexter" and then selects the genres not included in that list
+-- This script lists all genres not linked to
+-- the show "Dexter" from the hbtn_0d_tvshows database
+-- It retrieves the genres linked to the show "Dexter"
+-- and then selects the genres not included in that list
 -- Results are sorted in ascending order by the genre name
 
 SELECT tv_genres.name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.tv_show_id = shows.id
+LEFT JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 GROUP BY tv_genres.name;
 
