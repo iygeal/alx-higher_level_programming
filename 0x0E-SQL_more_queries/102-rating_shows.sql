ebe15db0e79dbfa1022ae83eb8c063937569df68
Innocent Anozie <iygeal@gmail.com>
2024-03-27 23:50:01 +0100

    Typo fix

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -5,7 +5,7 @@
 
 SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating_sum
 FROM tv_shows
-INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
 ORDER BY rating_sum DESC;
 
