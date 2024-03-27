e071014b92b9b1d30facf1fdadb903a382f582b8
Innocent Anozie <iygeal@gmail.com>
2024-03-27 01:08:26 +0100

    Refactored task 101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,9 @@
--- Calculate the average temperature (Fahrenheit)
--- by city and order by temperature (descending)
-
-SELECT city, AVG(temperature) AS average_temperature_fahrenheit
-FROM table_name
+-- Calculate the average temperature (Fahrenheit) by city and order by temperature (descending)
+-- Selects the city and calculates the average temperature, assigning it an alias avg_temp
+-- Specifies the table name (temperatures) from which to retrieve data
+-- Groups the results by city
+-- Orders the results by avg_temp (average temperature) in descending order
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
 GROUP BY city
-ORDER BY average_temperature_fahrenheit DESC;
+ORDER BY avg_temp DESC;
