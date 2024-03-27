64b880075f9145c5376fd65a38bb4361d177f0e3
Innocent Anozie <iygeal@gmail.com>
2024-03-27 22:44:39 +0100

    First use of join

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,12 @@
+-- This script lists all cities contained in the hbtn_0d_usa database
+-- along with their respective state names using JOIN
+-- It retrieves the city id and name from the cities table,
+-- and the corresponding state name from the states table using JOIN
+-- Results are sorted in ascending order by cities.id
+
+USE hbtn_0d_usa;
+
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
