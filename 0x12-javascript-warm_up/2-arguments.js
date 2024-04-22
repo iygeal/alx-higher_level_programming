0ffde6716fc5b81f861bca4eb53e8615ff6dfa90
Innocent Anozie <iygeal@gmail.com>
2024-04-22 16:02:11 +0100

    Fixed semistandard errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-ac = process.argv.length;
+const ac = process.argv.length;
 
 if (ac > 2) {
-  console.log("Argument" + (ac > 3 ? "s" : "") + " found");
+  console.log('Argument' + (ac > 3 ? 's' : '') + ' found');
 } else {
-  console.log("No argument");
+  console.log('No argument');
 }
