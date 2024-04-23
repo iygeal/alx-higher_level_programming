3958c17b33da5a8353b99d41022984fd38c83577
Innocent Anozie <iygeal@gmail.com>
2024-04-23 20:58:23 +0100

    Refactored

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -5,12 +5,14 @@
  */
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return {}; // Create an empty object if any condition is met
-    }
-    // Otherwise, initialize the properties of the rectangle
+    // Initialize instance attributes
     this.width = w;
     this.height = h;
+
+    // Check if w or h is not a positive integer or equal to 0
+    if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
+      return {}; // Return an empty object if conditions are met
+    }
   }
 }
 
