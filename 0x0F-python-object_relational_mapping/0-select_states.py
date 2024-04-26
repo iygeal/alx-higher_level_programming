f7397e06439cac6dd514458620b224cb8019eb36
Innocent Anozie <iygeal@gmail.com>
2024-04-26 07:36:54 +0100

    Pycodestyle fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,8 @@ from sys import argv
 
 if __name__ == "__main__":
     try:
-        states_db = MySQLdb.connect(
-            host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+        states_db = MySQLdb.connect(host="localhost", port=3306,
+                                    user=argv[1], passwd=argv[2], db=argv[3])
 
         cur = states_db.cursor()
         cur.execute("SELECT * FROM states ORDER BY id")
