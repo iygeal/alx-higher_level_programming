380cbcafa8d4bd92fe8c3d1b7dfeb4a00efa8e82
Innocent Anozie <iygeal@gmail.com>
2024-05-09 21:45:04 +0100

    Refactored code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Send a GET request to the URL and display the body of the response
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1"
+[[ "$(curl -so /dev/null -w "%{http_code}" "$1")" -eq 200 ]] && curl -s "$1"
+
