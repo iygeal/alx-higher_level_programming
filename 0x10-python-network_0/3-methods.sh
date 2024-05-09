c92a7d878f925b928b3b344ec092db50315e04ee
Innocent Anozie <iygeal@gmail.com>
2024-05-09 22:46:45 +0100

    Refactored allow to Allow

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" | grep -i "^allow:" | awk '{$1=""; print $0}'
+curl -sI "$1" | grep -i "^Allow:" | awk '{$1=""; print $0}'
