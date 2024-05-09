28331287f3a58811f46e38aba7c921eb5a67066a
Innocent Anozie <iygeal@gmail.com>
2024-05-09 22:48:23 +0100

    Refactored to use sed instead of awk

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" | grep -i "^Allow:" | awk '{$1=""; print $0}'
+curl -s -X OPTIONS -i "$1" | grep -i "^allow:" | sed 's/Allow: //i'
