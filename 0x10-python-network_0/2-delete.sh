2b45305288251c9cd177be4d1f92da102ffd6bb7
Innocent Anozie <iygeal@gmail.com>
2024-05-09 22:35:35 +0100

    Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response

---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script sends a DELETE request to the URL passed as the first argument and displays the body of the response
+
+# Send a DELETE request to the URL provided as the first argument,
+# and display the body of the response
+curl -sL -X DELETE "$1"
