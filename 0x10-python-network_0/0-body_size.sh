8f5383a6d80de06f052e31e71712c2a44d63a439
Innocent Anozie <iygeal@gmail.com>
2024-05-09 21:03:50 +0100

    Reduced to three lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# This script takes in a URL as an argument, sends a request to that URL using curl,
-# and displays the size of the body of the response in bytes
-
-# curl: command-line tool for transferring data with URLs
-# -s: Silent mode, suppresses progress meter and other output
-# -w "%{size_download}": Specifies a custom output format to print the size of the downloaded content
-# -o /dev/null: Redirects the output of the download to /dev/null, discarding the downloaded content
-# "$1": The URL provided as the first argument to the script, to which the request is sent
-curl -s -w "%{size_download}" -o /dev/null $1
+# This script takes in a URL as an argument, sends a request to that URL using curl, and displays the size of the body of the response in bytes
+curl -s -w "%{size_download}" -o /dev/null "$1"
