c6fea02314a74adc3ba1caf259361d65ec51dd6d
Innocent Anozie <iygeal@gmail.com>
2024-05-09 21:01:36 +0100

    Removed quotes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -7,4 +7,4 @@
 # -w "%{size_download}": Specifies a custom output format to print the size of the downloaded content
 # -o /dev/null: Redirects the output of the download to /dev/null, discarding the downloaded content
 # "$1": The URL provided as the first argument to the script, to which the request is sent
-curl -s -w "%{size_download}" -o /dev/null "$1"
+curl -s -w "%{size_download}" -o /dev/null $1
