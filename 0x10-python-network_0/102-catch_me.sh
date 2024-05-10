d422e3e65dbebad51b80e2a4c49c5926d4f5e626
Innocent Anoziee <iygeal@gmail.com>
2024-05-10 12:47:00 +0100

    Refactored to correct shebang

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # Make a request to 0.0.0.0:5000/catch_me and check the response
 curl -s http://0.0.0.0:5000/catch_me | grep -q "You got me!" && exit 0 || exit 1
