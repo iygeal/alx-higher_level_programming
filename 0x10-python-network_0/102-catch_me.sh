06355a02e8a1e2da0fd0510e7ce5858f5e34f054
Innocent Anoziee <iygeal@gmail.com>
2024-05-10 15:13:10 +0100

    Refactored

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a request to 0.0.0.0:5000/catch_me and check the response
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me >/dev/null 2>&1
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
