8aafd53adf19e952865162663ef0b9a5f41a0fb5
Innocent Anozie <iygeal@gmail.com>
2024-05-11 10:28:08 +0100

    Script that makes a POST request to a URL with an email and prints the response

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Python script that sends POST request and prints utf-8 response"""
+
+
+from sys import argv
+import urllib.request
+
+if __name__ == "__main__":
+
+    my_url, email = argv[1], argv[2]
+
+    email = email.encode('utf-8')
+
+    req = urllib.request.Request(my_url, email)
+
+    with urllib.request.urlopen(req) as response:
+        page_body = response.read()
+        decoded_page = page_body.decode('utf-8')
+    print(decoded_page)
