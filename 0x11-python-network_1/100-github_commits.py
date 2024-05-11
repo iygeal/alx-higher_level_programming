d1ab1aeb4397831b5faf93531d68ef6cb8fe30fa
Innocent Anozie <iygeal@gmail.com>
2024-05-11 23:55:56 +0100

    Refactored to use 'sha' as key to access commit hash

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,7 @@ import requests
 if __name__ == "__main__":
     repo, user = argv[1], argv[2]
 
-    url = f"https://api.github.com/repos{user}/{repo}/commits"
+    url = f"https://api.github.com/repos/{user}/{repo}/commits"
 
     # Send a GET request to the GitHub API endpoint
     response = requests.get(url)
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
         # Print the commits
         for commit in commits[:10]:  # Print the 10 most recent commits
-            commit_hash = commit['commit_hash']
+            commit_hash = commit['sha']
             author_name = commit['commit']['author']['name']
             print(f"{commit_hash}: {author_name}")
     else:
