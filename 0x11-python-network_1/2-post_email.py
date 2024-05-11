b0d6ee9033db3b5ddd56bcf5d19a87ff41edfc4a
Innocent Anozie <iygeal@gmail.com>
2024-05-11 11:02:50 +0100

    Moved print statement inside with block

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
 
     my_url, email = argv[1], {'email': argv[2]}
 
-    email = parse.urlencode(email).encode('ascii')
+    email = parse.urlencode(email).encode('utf-8')
 
-    req = request.Request(my_url, data=email)
+    req = request.Request(my_url, data=email, method="POST")
 
     with request.urlopen(req) as response:
         page_body = response.read()
         decoded_page = page_body.decode('utf-8')
-    print(decoded_page)
+        print(decoded_page)
