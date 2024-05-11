767e573d1781f6fdd05f9ac84812eb3a62b90e06
Innocent Anozie <iygeal@gmail.com>
2024-05-11 07:09:54 +0100

    Python script that fetches from a URL

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that fetches a url"""
+
+
+import urllib.request
+
+if __name__ == "__main__":
+    my_site = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(my_site) as response:
+        page_body = response.read()
+
+        page_decoded = page_body.decode('utf-8')
+
+    print("Body content:")
+    print("\t- type: ", type(page_body))
+    print("\t- content: ", page_body)
+    print("\t- utf8 content: ", page_decoded)
