5e570ac1c3ed9d97e67539215d973c50216f14d5
Innocent Anozie <iygeal@gmail.com>
2024-05-11 23:20:53 +0100

    Refactored

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,25 +5,20 @@
 
 
 from sys import argv
-import sys
 import requests
 
 if __name__ == "__main__":
-    try:
-        username, password = argv[1], argv[2]
+    username, password = argv[1], argv[2]
 
-        url = "https://api.github.com/user"
+    url = "https://api.github.com/user"
 
-        auth = (username, password)
+    auth = (username, password)
 
-        response = requests.get(url, auth=auth)
+    response = requests.get(url, auth=auth)
 
-        if response.status_code == 200:
-            user_data = response.json()
-            user_id = user_data['id']
-            print(user_id)
-        else:
-            print("Status code:", response.status_code)
-    except requests.exceptions.RequestException as e:
-        print("GitHub API inaccessible: code", e)
-        sys.exit(1)
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data['id']
+        print(user_id)
+    else:
+        print("Status code:", response.status_code)
