1d2cee32c2db513a4b93b9325ac77f1f907a227a
Innocent Anoziee <iygeal@gmail.com>
2024-06-05 16:34:21 +0100

    Pushed task 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const movieId = process.argv[2];
 
-const url = 'https://swapi-api.alx-tools.com/api/films/${movieId}';
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request.get(url, (err, response, body) => {
   if (err) {
